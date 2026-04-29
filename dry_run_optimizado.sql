35636676fb7645c5228389b5a270ee9a8c327526
WIlmer Vasquez <vasquezr.w98@gmail.com>
2026-04-28 21:44:32 -0600

    Mejoras en Readme

---
+++ dry_run_optimizado.sql
@@ -0,0 +1,1 @@
+SELECT * FROM `proyecto-2-seminario-sistemas.taxi_data.taxi_trips_2022_optimized` WHERE DATE(pickup_datetime) = '2022-01-15'
---
+++ dry_run_original.sql
@@ -0,0 +1,1 @@
+SELECT * FROM `bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2022` WHERE DATE(pickup_datetime) = '2022-01-15'
